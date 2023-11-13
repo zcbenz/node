919927c129d3c519f17e1a5247bd0583f94b331c
Cheng Zhao <zcbenz@gmail.com>
2023-11-13 22:01:07 +0900

    src: fix compatility with V8 11.10 APIs

--- src/js_native_api_v8.cc
+++ src/js_native_api_v8.cc
@@ -971,8 +971,7 @@ napi_define_class(napi_env env,
       tpl->PrototypeTemplate()->SetAccessorProperty(property_name,
                                                     getter_tpl,
                                                     setter_tpl,
-                                                    attributes,
-                                                    v8::AccessControl::DEFAULT);
+                                                    attributes);
     } else if (p->method != nullptr) {
       v8::Local<v8::FunctionTemplate> t;
       STATUS_CALL(v8impl::FunctionCallbackWrapper::NewTemplate(
