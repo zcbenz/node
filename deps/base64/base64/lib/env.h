e4a552d9bb302d18db8c2202d227349b716b29d7
Cheng Zhao <zcbenz@gmail.com>
2023-11-13 12:26:25 +0900

    build: fix GN configuration for deps/base64

--- deps/base64/base64/lib/env.h
+++ deps/base64/base64/lib/env.h
@@ -46,12 +46,12 @@
 #if defined (__x86_64__)
 // This also works for the x32 ABI, which has a 64-bit word size.
 #  define BASE64_WORDSIZE 64
-#elif defined (_INTEGRAL_MAX_BITS)
-#  define BASE64_WORDSIZE _INTEGRAL_MAX_BITS
 #elif defined (__WORDSIZE)
 #  define BASE64_WORDSIZE __WORDSIZE
 #elif defined (__SIZE_WIDTH__)
 #  define BASE64_WORDSIZE __SIZE_WIDTH__
+#elif defined (_INTEGRAL_MAX_BITS) && !defined(_WIN32)
+#  define BASE64_WORDSIZE _INTEGRAL_MAX_BITS
 #else
 #  error BASE64_WORDSIZE_NOT_DEFINED
 #endif
